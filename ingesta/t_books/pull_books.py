581262542b7c6067989b07f15089688858f2a440
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-11-23 19:39:51 -0500

    fix: pull_books.py with stages

--- ingesta/t_books/pull_books.py
+++ ingesta/t_books/pull_books.py
@@ -2,17 +2,27 @@ import boto3
 import csv
 import os
 
-def export_table_to_csv_dynamodb(table_name, output_dir="clientes_db"):
-    # Crear cliente DynamoDB
-    dynamodb = boto3.client('dynamodb')
+# Variable global para definir el nombre de la tabla
+TABLE_NAME = "t_books"
+
+def export_table_to_csv_dynamodb(prefix, table_name=TABLE_NAME):
+    """
+    Exporta los datos de una tabla DynamoDB a un archivo CSV sin encabezados.
     
+    Args:
+        prefix (str): Prefijo del directorio donde se guardará el archivo (dev, test, prod).
+        table_name (str): Nombre de la tabla DynamoDB.
+    """
+    # Crear cliente DynamoDB
+    dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+
     # Crear directorio de salida si no existe
-    table_dir = os.path.join(output_dir, table_name)
-    if not os.path.exists(table_dir):
-        os.makedirs(table_dir)
+    output_dir = f"./{prefix}-{table_name}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Definir ruta del archivo CSV
-    csv_file_path = os.path.join(table_dir, f"{table_name}.csv")
+    csv_file_path = os.path.join(output_dir, f"{table_name}.csv")
 
     # Abrir el archivo CSV para escribir
     with open(csv_file_path, mode='w', newline='') as file:
@@ -33,5 +43,7 @@ def export_table_to_csv_dynamodb(table_name, output_dir="clientes_db"):
 
     print(f"Exported table {table_name} to {csv_file_path}")
 
-# Uso de la función
-export_table_to_csv_dynamodb("test-t_books")
+# Llamadas a la función con diferentes prefijos
+export_table_to_csv_dynamodb("dev")
+export_table_to_csv_dynamodb("test")
+#export_table_to_csv_dynamodb("prod")
